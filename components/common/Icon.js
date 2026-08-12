d7cea303dc0efd7ecbd6ffdf32943471f1fe6ad4
ZED-09 <farhatanzidan09@gmail.com>
2026-08-12 08:27:32 +0700

    1

--- components/common/Icon.js
+++ components/common/Icon.js
@@ -68,6 +68,7 @@ import {
   FaQrcode,
   FaMobile,
   FaMoneyBillWave,
+  FaScrewdriverWrench,
 } from "react-icons/fa6";
 
 const icons = {
@@ -231,6 +232,7 @@ const icons = {
   qrcode: FaQrcode,
   mobile: FaMobile,
   money: FaMoneyBillWave,
+  jasa: FaScrewdriverWrench,
   google: (
     <svg viewBox="0 0 24 24">
       <path fill="#4285F4" d="M23.49 12.27c0-.79-.07-1.54-.19-2.27H12v4.51h6.47a5.57 5.57 0 0 1-2.4 3.58v3h3.86c2.26-2.09 3.56-5.17 3.56-8.82z" />
